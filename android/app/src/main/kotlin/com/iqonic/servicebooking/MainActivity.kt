ea4bad806d6642dd45ccada51c94eec1f56d14db
Prasad <sawantp500@gmail.com>
2024-11-30 21:59:04 +0530

    bugs fixed

--- android/app/src/main/kotlin/com/iqonic/servicebooking/MainActivity.kt
+++ android/app/src/main/kotlin/com/iqonic/servicebooking/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.iqonic.servicebooking
+package com.okmaintenance.servicebooking
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
